c18a1167c6fad983872945133f66bfb755f8df4c
Fadlan <arikamusudahmakantara@gmail.com>
2026-08-21 14:42:10 +0700

    update

--- pulsa-fe/app/(auth)/login/page.tsx
+++ pulsa-fe/app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { LoginCard } from "@/components/auth/LoginCard";
 import { BackgroundAuth } from "@/components/auth/BackgroundAuth";
 
 export default function LoginPage() {
   return (
     <BackgroundAuth>
-      <LoginCard />
+      <Suspense fallback={<div className="min-h-[620px] w-full" aria-hidden="true" />}>
+        <LoginCard />
+      </Suspense>
     </BackgroundAuth>
   );
 }
