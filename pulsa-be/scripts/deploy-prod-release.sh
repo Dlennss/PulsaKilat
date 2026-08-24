b03e6721b4278d070b4501306b96edcc9550a40d
Fadlan <arikamusudahmakantara@gmail.com>
2026-08-24 16:57:55 +0700

    update

--- pulsa-be/scripts/deploy-prod-release.sh
+++ pulsa-be/scripts/deploy-prod-release.sh
@@ -88,6 +88,7 @@ ln -sfn "$SHARED_LOG_DIR" "$BUILD_DIR/log"
 cd "$BUILD_DIR"
 go run ./scripts/apply_sql_migration sql/20260808_retail_withdraw_funding_source.sql
 go run ./scripts/apply_sql_migration sql/20260821_add_marketing_agent_relation.sql
+go run ./scripts/apply_sql_migration sql/20260824_agent_credit_flexible_limit.sql
 go test ./internal/service ./internal/provider ./internal/helper ./chytron ./loketbayar ./smb ./rajabiller
 go build -buildvcs=false -o "$BUILD_DIR/pulsa-be" .
 
